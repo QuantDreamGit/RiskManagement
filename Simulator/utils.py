a3dba0bee26e2b962169040d84b38f5de9123f80
quantdream <s283085@studenti.polito.it>
2025-01-17 17:37:04 +0100

    removed unecessary settings

--- Simulator/utils.py
+++ Simulator/utils.py
@@ -1,7 +1,7 @@
 import os
 import gurobipy as gp
 
-def set_environment(threads=24, output_flag=0, node_method=2, concurrent_mip=1):
+def set_environment(threads=24, output_flag=0):
     options = {
         "WLSACCESSID": os.getenv("WLSACCESSID"),
         "WLSSECRET": os.getenv("WLSSECRET"),
@@ -10,8 +10,6 @@ def set_environment(threads=24, output_flag=0, node_method=2, concurrent_mip=1):
 
     env = gp.Env(params=options)
     env.setParam("OutputFlag", output_flag)
-    env.setParam("Threads", threads)
-    env.setParam('NodeMethod', node_method)
-    env.setParam('ConcurrentMIP', concurrent_mip)
+    # env.setParam("Threads", threads)
 
     return env
